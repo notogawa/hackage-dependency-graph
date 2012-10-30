e286c30b6dc784a954989049b0c7c750fe71244d
notogawa <n.ohkawa@gmail.com>
2012-10-30 22:37:02 +0900

    optimize.

--- updater.hs
+++ updater.hs
@@ -10,7 +10,8 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 import Text.GEXF
 import Text.XML
-import Data.List ( intercalate, nub, sort )
+import Data.List ( intercalate, nub, sort, groupBy )
+import Data.Function ( on )
 import Cabal
 import Distribution.Package
 import Distribution.Version
@@ -34,16 +35,20 @@ main = packageGraph
 a # b = pkgName a == pkgName b
 
 packageGraph = do
-  packages <- getPackageDirs >>= getPackages >>= mapM getPackageInfo
+  packages <- sort <$> getPackageDirs >>= getPackages >>= mapM getPackageInfo
   let nodes = zip [0..] $ nub $ map (pkgName . fst) packages
-      nmap = M.fromList $ map (uncurry (flip (,))) nodes
+      package2node = M.fromList $ map (uncurry (flip (,))) nodes
+      pkgName2packages =
+          M.fromList [ (pkgName $ fst $ head ps, ps)
+                     | ps <- groupBy ((==) `on` (pkgName . fst)) packages
+                     ]
       edges = nub . sort . concat $ do
                  (package, dependencies) <- packages
-                 let Just src = M.lookup (pkgName package) nmap
+                 let Just src = M.lookup (pkgName package) package2node
                  return [ (src, dst, ())
                         | dependency <- dependencies
-                        , (p, _) <- searchPackage dependency packages
-                        , let Just dst = M.lookup (pkgName p) nmap
+                        , (p, _) <- searchPackage dependency pkgName2packages
+                        , let Just dst = M.lookup (pkgName p) package2node
                         ]
       graph = G.mkGraph nodes edges :: PackageGraph
   writeFile def "hackage.gexf" $ toDocument (T.pack . showPackageName) graph
@@ -51,14 +56,18 @@ packageGraph = do
 packageVersionGraph = do
   packages <- getPackageDirs >>= getPackages >>= mapM getPackageInfo
   let nodes = zip [0..] $ map fst packages
-      nmap = M.fromList $ map (uncurry (flip (,))) nodes
+      package2node = M.fromList $ map (uncurry (flip (,))) nodes
+      pkgName2packages =
+          M.fromList [ (pkgName $ fst $ head ps, ps)
+                     | ps <- groupBy ((==) `on` (pkgName . fst)) packages
+                     ]
       edges = concat $ do
                  (package, dependencies) <- packages
-                 let Just src = M.lookup package nmap
+                 let Just src = M.lookup package package2node
                  return [ (src, dst, ())
                         | dependency <- dependencies
-                        , (p, _) <- searchPackage dependency packages
-                        , let Just dst = M.lookup p nmap
+                        , (p, _) <- searchPackage dependency pkgName2packages
+                        , let Just dst = M.lookup p package2node
                         ]
       graph = G.mkGraph nodes edges :: PackageVersionGraph
   writeFile def "hackage.full.gexf" $ toDocument (T.pack . showPackage) graph
@@ -78,8 +87,10 @@ getPackages packageDirs =
 getPackageInfo (packageDir, versionDir) =
     parseCabalFile (hackageDir ++ "/"++packageDir++"/"++versionDir++"/"++packageDir++".cabal")
 
-searchPackage (Dependency packageName versionRange) =
-    filter (matchPackage packageName versionRange)
+searchPackage (Dependency packageName versionRange) pkgName2packages =
+    case M.lookup packageName pkgName2packages of
+      Nothing -> []
+      Just packages -> filter (matchPackage packageName versionRange) packages
 
 matchPackage packageName versionRange (package, _deps) =
     packageName == pkgName package &&
